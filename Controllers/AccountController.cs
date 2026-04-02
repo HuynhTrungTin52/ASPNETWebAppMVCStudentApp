25071c90e904f9b8531ac8b1832a4836b0cb7b1b
HuynhTrungTin52 <524k0033@student.tdtu.edu.vn>
2026-04-03 00:34:30 +0700

    Implemented a login page, created a report file but has not completed the report view

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using ASPNETWebAppMVCStudentApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace ASPNETWebAppMVCStudentApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SchoolDBEntities db = new SchoolDBEntities();
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(tblUser user)
+        {
+            var obj = db.tblUsers.Where(a => a.Username.Equals(user.Username) && a.Password.Equals(user.Password)).FirstOrDefault();
+
+            if (obj != null)
+            {
+                Session["UserID"] = obj.UserID.ToString();
+                Session["Username"] = obj.Username.ToString();
+
+                return RedirectToAction("Index", "Students");
+            }
+
+            ViewBag.Message = "Invalid Username or Password. Please try again or reset your password.";
+            return View(user);
+        }
+    }
+}
